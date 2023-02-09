0108b1f42b8dd5a7cd0ad0c0501c76d70f095450
TESTID <52897037+Mrhoony@users.noreply.github.com>
2023-02-09 16:50:43 +0900

    Create Listen Server

---
+++ Source/Game/CMovingPlatform.cpp
@@ -0,0 +1,18 @@
+#include "CMovingPlatform.h"
+
+ACMovingPlatform::ACMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetMobility(EComponentMobility::Movable);
+}
+
+void ACMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector location = GetActorLocation();
+	location += FVector(Speed * DeltaTime, 0, 0);
+
+	SetActorLocation(location);
+}
